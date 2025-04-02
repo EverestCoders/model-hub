2e30017fad2078780703e1c6ba86a42314afa382
utnim2 <mintugogoi567@gmail.com>
2025-04-02 15:29:05 +0530

    fix: auth middleware is working now instead of hardcoded user id, but user needs to be connected to a wallet

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,6 +1,6 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
-import config from '../config/env';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import config from "../config/env";
 
 export interface AuthRequest extends Request {
   user?: {
@@ -9,32 +9,33 @@ export interface AuthRequest extends Request {
   };
 }
 
-export const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
-  req.user = {
-    id: "1f8bacf8-b719-496f-a996-b3d0f2c79f44",
-    walletAddress: "0x8945dede1d19a582381c88d1ee6fe5e503d65f56"
-  };
+// req.user = {
+//   id: "1f8bacf8-b719-496f-a996-b3d0f2c79f44",
+//   walletAddress: "0x8945dede1d19a582381c88d1ee6fe5e503d65f56"
+// };
 
-  next()
+export const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction): void => {
   // Get the token from the header
-  // const token = req.headers.authorization?.split(' ')[1];
+  const token = req.headers.authorization?.split(' ')[1];
   
-  // if (!token) {
-  //   return res.status(401).json({ error: 'No token provided' });
-  // }
+  if (!token) {
+    res.status(401).json({ error: 'No token provided' });
+    return;
+  }
   
-  // try {
-  //   // Verify token
-  //   const decoded = jwt.verify(token, config.jwtSecret) as { id: string; walletAddress: string };
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, config.jwtSecret) as { id: string; walletAddress: string };
     
-  //   // Add user from payload to request
-  //   req.user = {
-  //     id: decoded.id,
-  //     walletAddress: decoded.walletAddress
-  //   };
+    // Add user from payload to request
+    req.user = {
+      id: decoded.id,
+      walletAddress: decoded.walletAddress
+    };
     
-  //   next();
-  // } catch (error) {
-  //   return res.status(401).json({ error: 'Invalid token' });
-  // }
-};
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Invalid token' });
+    return;
+  }
+}
